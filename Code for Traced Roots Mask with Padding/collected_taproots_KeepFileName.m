904129b5d1ceead535c92f8c14d56c87c0dd62dd
SoylabSingh <70410973+SoylabSingh@users.noreply.github.com>
2022-06-28 17:22:47 -0500

    Add files via upload
    Code needed to create manual taproot masks for use in SNAP if auto-generated taproot detection produces a poor quality mask.

---
+++ Code for Traced Roots Mask with Padding/collected_taproots_KeepFileName.m
@@ -0,0 +1,26 @@
+clear all
+clc
+% filelist with extension *.jpg
+filelist=dir('Traced Roots/*.jpg');
+% create output directory
+output_directory = 'traced_tap_binary_padded';
+mkdir(output_directory);
+
+patchsize = 256;
+for i=1:length(filelist)
+    if endsWith(filelist(i).name,'.jpg')
+        Im=imread(fullfile(filelist(i).folder,filelist(i).name));
+        size(Im)
+        filenamea=erase(filelist(i).name,'.jpg');
+        %filenameb=erase(filenamea,'Inked');
+        % identifying tap root
+        [BWa,~]=createMask_tap(Im);
+        % add padding
+        [h1,w1]=size(BWa);
+        pad_r = ceil(h1/patchsize)*patchsize-h1;
+        pad_c = ceil(w1/patchsize)*patchsize-w1;
+        BW = padarray(BWa,[pad_r pad_c],0,'post');
+        % save image
+        imwrite(BW,fullfile(output_directory,strcat(filenamea,'.png')));
+    end
+end
